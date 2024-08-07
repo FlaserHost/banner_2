51d747e606b449656ab2eb6f1209b650b220a2a8
Терленев Дмитрий <terlenev.dk@office.volga-soft.ru>
2024-08-07 10:28:57 +0300

    init

---
+++ src/js/script.js
@@ -0,0 +1,28 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+    if (!sessionStorage.bannerWatched) {
+        //setTimeout(() => {
+            const modal = document.querySelector('.modal');
+            modal.style.display = 'flex';
+            document.body.style.overflow = 'hidden';
+
+            const closeBtn = modal.querySelector('#banner-close-btn');
+            closeBtn.addEventListener('click', e => {
+                e.target.closest('.modal').remove();
+                sessionStorage.bannerWatched = true;
+                document.body.style.overflow = 'visible';
+            });
+
+            const modalBtns = modal.querySelectorAll('.modal-btn');
+            modalBtns.forEach(btn => {
+                btn.addEventListener('click', e => {
+                    e.preventDefault();
+                    sessionStorage.bannerWatched = true;
+                    const href = e.target.getAttribute('href');
+                    location.href = href;
+                });
+            });
+        //}, 15000);
+    }
+});
